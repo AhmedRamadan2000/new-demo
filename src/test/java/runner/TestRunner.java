c0cd87cfb736bf510999ab34d91c4be1b42ab570
Ahmed Ramadan <95707983+AhmedRamadan2000@users.noreply.github.com>
2023-03-31 05:54:46 +0200

    Add login test cases with cucumber

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,8 +1,11 @@
 package runner;
 
 import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
 import tests.TestBase;
 
+@RunWith(Cucumber.class)
 @CucumberOptions (features = "src/test/java/features/UserLogin.feature", glue = {"steps"},
       plugin = {"pretty","html:target/cucmber-html-report"})
 public class TestRunner extends TestBase{
--- src/test/java/steps/UserLoginSteps.java
+++ src/test/java/steps/UserLoginSteps.java
@@ -2,7 +2,7 @@ package steps;
 
 import io.cucumber.java.en.Given;
 
-public class UserLogin {
+public class UserLoginSteps {
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {

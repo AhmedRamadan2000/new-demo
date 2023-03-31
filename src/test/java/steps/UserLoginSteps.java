999a672383fc4e409d56df90d0b0e05f9cbee543
Ahmed Ramadan <95707983+AhmedRamadan2000@users.noreply.github.com>
2023-03-31 06:04:51 +0200

    Add login Steps with cucumber

--- src/test/java/steps/UserLoginSteps.java
+++ src/test/java/steps/UserLoginSteps.java
@@ -1,12 +1,46 @@
 package steps;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class UserLoginSteps {
 
+    WebDriver driver;
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
+        driver = new ChromeDriver();
+        driver.get("https://example.com/login");
         throw new io.cucumber.java.PendingException();
     }
+    @When("I enter valid credentials")
+    public void i_enter_valid_credentials() {
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        usernameField.sendKeys("myusername");
+        passwordField.sendKeys("mypassword");
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("click on the login button")
+    public void click_on_the_login_button() {
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be redirected to the overview page")
+    public void i_should_be_redirected_to_the_overview_page() {
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals("https://example.com/home", currentUrl);
+        driver.quit();
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
